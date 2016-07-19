4e9e4dd30b9cdb59f22920917eb0083c5ac234f7
Jack Mocherman <mochermanjack@gmail.com>
2016-07-19 09:18:11 -0500

    four day/night forecast populates dom in a list

--- js/getWeather.js
+++ js/getWeather.js
@@ -1,14 +1,15 @@
-// "use strict";
+"use strict";
 
-// let $ = require('jquery');
+let $ = require('jquery');
 
-// function getWeather(zip) {
-//   console.log(zip);
-//   // return new Promise(function(resolve, reject) {
-//   //   $.ajax({
-//   //     url: `http://api.wunderground.com/api/cadf2ec036697adc/conditions/q/.json`
-//   //   })
-//   // })
-// }
+function getWeather(zip) {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: `http://api.wunderground.com/api/cadf2ec036697adc/forecast/q/${zip}.json`
+    }).done(function(forecast) {
+      resolve(forecast.forecast.txt_forecast.forecastday);
+    })
+  })
+}
 
-// module.exports = getWeather;
+module.exports = getWeather;
--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $("#auth-btn").click(function() {
     .then(function(result) {
       user = result.user;
       currentUser.setUser(user.uid);
-      templates();
+      templates.domBuild();
     })
     .catch(function(error) {
       // Handle Errors here.
@@ -26,36 +26,41 @@ $("#auth-btn").click(function() {
     });
 });
 
-// $(document).on('click', '#submit-btn', function() {
-//   let zip = $('#input').val();
-//   validateZip(zip)
-//     .then(function(data) {
-//       if (data) {
-//         getWeather(zip);
-//       } else {
-//         alert('Please use 5 digit zip');
-//       }
-//     });
-// });
+$(document).on('click', '#submit-btn', function() {
+  let zip = $('#input').val();
+  validateZip(zip)
+    .then(function(data) {
+      if (data) {
+        getWeather(zip)
+          .then(function(forecast) {
+            templates.outputForecast(forecast);
+          });
+      } else {
+        alert('Please use 5 digit zip');
+      }
+    });
+});
 
-// $(document).on('keypress', '#input', (function(e) {
-//   let key = e.which,
-//     let zip = $('#input').val();
-//   if (key == 13) // the enter key code
-//   {
-//     validateZip(zip)
-//       .then(function(data) {
-//         if (data) {
-//           getWeather(zip)
-//         } else {
-//           alert('Please use 5 digit zip');
-//         }
-//       });;
-//   }
-// }));
+$(document).on('keypress', '#input', (function(e) {
+  let key = e.which,
+    zip = $('#input').val();
+  if (key == 13) // the enter key code
+  {
+    validateZip(zip)
+      .then(function(data) {
+        if (data) {
+          getWeather(zip)
+            .then(function(forecast) {
+              templates.outputForecast(forecast);
+            });
+        } else {
+          alert('Please use 5 digit zip');
+        }
+      });;
+  }
+}));
 
 function validateZip(zip) {
-  console.log("test");
   return new Promise(function(resolve, reject) {
     resolve(/^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(zip));
   });
